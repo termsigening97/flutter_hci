3ea0b7aae80e5a8bf24663e1ffc64bf935541500
Jun Hwi Gu <63348675+termsigening97@users.noreply.github.com>
2023-05-21 22:26:17 +0900

    Vocabulary Canceled

--- lib/voca.dart
+++ lib/voca.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Menu extends StatefulWidget {
-  const Menu({Key? key}) : super(key: key);
+class VocabularyScreen extends StatefulWidget {
+  const VocabularyScreen({Key? key}) : super(key: key);
 
   @override
-  State<Menu> createState() => _MenuState();
+  State<VocabularyScreen> createState() => _VocabularyScreenState();
 }
 
-class _MenuState extends State<Menu> {
+class _VocabularyScreenState extends State<VocabularyScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Column(children: [
+      Container(
+        color: Colors.lightBlue,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                '한영사전',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: const Icon(Icons.menu, size: 32),
+            )
+          ]),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              textAlignVertical: TextAlignVertical.center,
+              onTap: () {
+                Navigator.pop(context);
+              },
+              textInputAction: TextInputAction.search,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                suffixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    ]));
   }
 }
